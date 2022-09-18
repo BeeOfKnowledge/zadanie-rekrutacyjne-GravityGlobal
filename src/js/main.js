2371aa9f7e0a224c24dd8075cb21467ea0ca0b09
Oleksii Hudkov <oleksiihudkov@gmail.com>
2022-09-18 19:52:35 +0200

    feat: add reset button in the popup menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,45 @@
+var clicks = 0;
+
 window.onload = function () {
   document.getElementById("btn-popup").addEventListener("click", showPopup);
+  document.getElementById("popup__icon").addEventListener("click", closePopup);
+  document.getElementById("btn--popup").addEventListener("click", resetClicks);
+
+  if (localStorage.getItem("clicks")) {
+    clicks = Number(localStorage.getItem("clicks"));
+  }
 };
 
 function showPopup() {
   document.getElementById("overlay").classList.remove("hide");
   document.getElementById("popup").classList.remove("hide");
+
+  addClick();
+
+  if (clicks > 5) {
+    document.getElementById("btn--popup").classList.remove("hide");
+  }
+}
+
+function closePopup() {
+  document.getElementById("overlay").classList.add("hide");
+  document.getElementById("popup").classList.add("hide");
+}
+
+function addClick() {
+  clicks += 1;
+  var b = document.getElementById("popup__clicks");
+  b.textContent = clicks + " times";
+
+  localStorage.setItem("clicks", clicks);
+}
+
+function resetClicks() {
+  localStorage.clear();
+  localStorage.setItem("clicks", 0);
+  clicks = 0;
+
+  var b = document.getElementById("popup__clicks");
+  b.textContent = 0 + " times";
+  document.getElementById("btn--popup").classList.add("hide");
 }
